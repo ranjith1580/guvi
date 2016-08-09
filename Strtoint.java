710205fa60aa7e8d7207fb754e15843661b0f4eb
ranjith1580 <ranjith1580@gmail.com>
2016-08-09 17:59:54 +0530

    Add files via upload

---
+++ Strtoint.java
@@ -0,0 +1,14 @@
+import java.io.*;
+import java.util.*;
+class Strtoint
+{
+  	public static void main(String[] args) 
+  	{
+  	Scanner sc=new Scanner(System.in);
+	int n;
+	String s=sc.next();
+	n=Integer.parseInt(s);
+	System.out.println(n);
+
+	}
+}
